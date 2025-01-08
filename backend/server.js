af562f6f730660877eb7e87b78252aa0a7312586
shahid <ms4811879@gmail.com>
2025-01-08 17:09:34 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const cors=require('cors')
 app.use(express.json())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://spotify-clone-rose-seven-51.vercel.app",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"]
