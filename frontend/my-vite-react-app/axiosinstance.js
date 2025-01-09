a0e287983f0d14af043c279f90e6c19e85c1d376
shahid <ms4811879@gmail.com>
2025-01-09 09:56:34 +0530

    backend url changed

--- frontend/my-vite-react-app/axiosinstance.js
+++ frontend/my-vite-react-app/axiosinstance.js
@@ -4,7 +4,7 @@ const token=localStorage.getItem('token')
 
 console.log(token);
 const axiosInstance=axios.create({
-    baseURL:'http://localhost:3001',
+    baseURL:'https://spotify-clone-rtqp.onrender.com',
     withCredentials:true,
     headers:{
         Authorization:`Bearer ${token}`
