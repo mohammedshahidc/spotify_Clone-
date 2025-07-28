e07e805c7e6a92c32f2c7363f5ec1f0b67b0bdfb
shahid <ms4811879@gmail.com>
2025-07-28 17:14:56 +0530

    final push

--- frontend/my-vite-react-app/axiosinstance.js
+++ frontend/my-vite-react-app/axiosinstance.js
@@ -4,8 +4,8 @@ const token=localStorage.getItem('token')
 
 console.log(token);
 const axiosInstance=axios.create({
-    // baseURL:'https://spotify-clone-rtqp.onrender.com',
-    baseURL:'http://localhost:3001',
+    baseURL:'https://spotify-clone-rtqp.onrender.com',
+    // baseURL:'http://localhost:3001',
     withCredentials:true,
     headers:{
         Authorization:`Bearer ${token}`
